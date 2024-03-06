bf23b15091931417092dd8de9676665dc35ef0f9
david97 <did625@yonsei.ac.kr>
2024-03-06 19:35:10 +0900

    all Files

--- apache24/htdocs/testpy.py
+++
@@ -1,5 +0,0 @@
-#!"C:\Users\ssss\AppData\Local\Programs\Python\Python38\python.exe"
-# -*- coding: utf-8 -*-
-import sys
-
-print(sys.argv[2]+" " "modelname"+ " " + "grade" + " " + "blahblah" +" ")
--- project/app/src/main/java/com/example/jungcar/QrCapture.java
+++
@@ -1,7 +0,0 @@
-package com.example.jungcar;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-public class QrCapture extends CaptureActivity {
-
-}
